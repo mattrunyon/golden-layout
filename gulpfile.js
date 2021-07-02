b558d7f205c0348087353e352815fe1a82fcbc7c
Matthew Runyon <matthewrunyon@deephaven.io>
2021-07-02 18:49:52 -0500

    Removed grunt from build and dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,47 @@
 // Deprecated but left here for now; use the gulp tasks in the Gruntfile.
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var cConcat = require('gulp-continuous-concat');
-var uglify = require('gulp-uglify');
-var insert = require('gulp-insert');
-var watch = require('gulp-watch');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var insert = require("gulp-insert");
+var path = require("path");
 
-gulp.task( 'dev', function() {
-	return gulp
-	.src([
-		'./build/ns.js',
-		'./src/js/utils/utils.js',
-		'./src/js/utils/EventEmitter.js',
-		'./src/js/utils/DragListener.js',
-		'./src/js/**'
-	])
-	.pipe(watch('./src/js/**'))
-	.pipe(cConcat('goldenlayout.js'))
-	.pipe(insert.wrap('(function($){', '})(window.$);' ))
-	.pipe(gulp.dest('./dist'));
-});
+const srcFiles = [
+  "./build/ns.js",
+  "./src/js/utils/utils.js",
+  "./src/js/utils/EventEmitter.js",
+  "./src/js/utils/DragListener.js",
+  "./src/js/**",
+];
 
-gulp.task( 'build', function() {
-	return gulp
-	.src([
-		'./build/ns.js',
-		'./src/js/utils/utils.js',
-		'./src/js/utils/EventEmitter.js',
-		'./src/js/utils/DragListener.js',
-		'./src/js/**'
-	])
-	.pipe(concat('goldenlayout.js'))
-	.pipe(insert.wrap('(function($){', '})(window.$);' ))
-	.pipe(gulp.dest('./dist'))
-	.pipe(uglify())
-	.pipe(concat('goldenlayout.min.js'))
-	.pipe(gulp.dest('./dist'));
-});
+const outputFile = "goldenlayout.js";
+const outputMinFile = "goldenlayout.min.js";
+const outputFolder = "./dist";
+
+function watch() {
+  return gulp.watch(srcFiles, combine);
+}
+
+function combine() {
+  return (
+    gulp
+      // Source files loaded in order
+      .src(srcFiles)
+      // Concatenate to goldenLayout.js
+      .pipe(concat(outputFile))
+      // These are a prefix and suffix added to the concatenated file
+      .pipe(insert.wrap("(function($){", "})(window.$);"))
+      .pipe(gulp.dest(outputFolder))
+  );
+}
+
+function minify() {
+  return gulp
+    .src(path.join(outputFolder, outputFile))
+    .pipe(uglify())
+    .pipe(concat(outputMinFile))
+    .pipe(gulp.dest(outputFolder));
+}
+
+exports.build = gulp.series(combine, minify);
+exports.dev = gulp.series(combine, watch);
+exports.default = exports.build;
